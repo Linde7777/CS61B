4e8ea6f0b304b4d4072a7fedac11bd76a094cf87
LongPeng <longpeng96@qq.com>
2021-11-22 19:46:47 +0800

    final version

--- lab2/DebugPractice/DebugExercise2.java
+++ lab2/DebugPractice/DebugExercise2.java
@@ -21,47 +21,39 @@ public class DebugExercise2 {
       * This function may have a bug, but if it does, you should find it
       * by stepping over, not into. */    
     public static int add(int a, int b) {
-        int x = a, y = b;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int xor, and, temp;
-        and = x & y;
-        xor = x ^ y;
-
-        while (and != 0) {
-            and <<= 1;
-            temp = xor ^ and;
-            and &= xor;
-            xor = temp;
-        }
-        return xor;
+        return a+b;
     }
 
     /** Returns a new array where entry i is the max of
      * a[i] and b[i]. For example, if a = {1, -10, 3}
      * and b = {0, 20, 5}, this function will return {1, 20, 5}.
      * */
-    public static int[] arrayMax(int[] a, int[] b) {
-        if (a.length != b.length) {
-            System.out.println("ERROR! Arrays don't match");
-            return null;
-        }
-        int[] returnArray = new int[a.length];
-        for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = max(a[i], b[i]);
-            returnArray[i] = biggerValue;
-        }
 
-        return returnArray;
+    //here is a bug
+    public static int[] arrayMax(int[] a, int[] b) {
+        int[] max=new int[a.length];
+
+        if(a.length==b.length) {
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] > b[i]) {
+                    max[i] = a[i];
+                } else {
+                    max[i] = b[i];
+                }
+            }
+            return max;
+
+        }else{
+            System.out.println("Wrong input");
+            return max;
+        }
     }
 
     /** Returns the sum of all elements in x. */
     public static int arraySum(int[] x) {
-        int i = 0;
-        int sum = 0;
-        while (i < x.length) {
-            sum = sum + add(sum, x[i]);
-            i = i + 1;
+        int sum=0;
+        for(int i=0;i<x.length;i++){
+            sum+=x[i];
         }
         return sum;
     }
