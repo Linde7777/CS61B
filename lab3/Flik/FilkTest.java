f00bffdf348201f47c1a5e04e541c156969b28e3
LongPeng <longpeng96@qq.com>
2022-01-16 09:41:33 +0800

    bug fixed

--- lab3/Flik/FilkTest.java
+++ lab3/Flik/FilkTest.java
@@ -4,6 +4,30 @@ public class FilkTest {
 
     @Test
     public void testFilk() {
+        Integer a = 3;
+        Integer b = 5;
+        boolean input;
+        boolean expected = false;
+
+        input = Flik.isSameNumber(a, b);
+        if (input == expected) {
+            System.out.println("Test1 pass");
+        } else {
+            System.out.println("Test1 fail");
+        }
+
+
+        Integer c = 129;
+        Integer d = 129;
+        boolean input1;
+        boolean expected1 = true;
+
+        input1 = Flik.isSameNumber(c, d);
+        if (input1 == expected1) {
+            System.out.println("Test2 pass");
+        } else {
+            System.out.println("Test2 fail");
+        }
     }
 
 }
