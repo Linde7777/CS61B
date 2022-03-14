2690bcf7e5643a4d6604baa4c9300155f8123cb6
LongPeng <longpeng96@qq.com>
2022-03-14 14:41:14 +0800

    open() completed

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -5,41 +5,29 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     private int N;
     WeightedQuickUnionUF wqu;
-    private final int blockedSite = 0;
-    private final int fullOpenSite = 1;
-    private final int emptyOpenSite = 2;
+    int[][] openHelper;
+    final int isBlocked = 0;
+    final int isOpened = 1;
 
 
     public Percolation(int N) {// create N-by-N grid, with all sites initially blocked
         if (N <= 0) {
             throw new java.lang.IllegalArgumentException();
         }
-
         this.N = N;
         wqu = new WeightedQuickUnionUF(N * N);
-    }
 
-    private void testXyTo1D() {
-        if (xyTo1D(4, 2, 3) == 14) {
-            System.out.println("test 1 passed");
+        openHelper = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                openHelper[i][j] = isBlocked;
+            }
         }
-        if (xyTo1D(4, 3, 0) == 3) {
-            System.out.println("test 2 passed");
-        }
-        if(xyTo1D(4,0,3)==12){
-            System.out.println("test 3 passed");
-        }
-        if(xyTo1D(2,1,1)==3){
-            System.out.println("test 4 passed");
-        }
+
     }
 
     public int xyTo1D(int x, int y) {
-        return xyTo1D(this.N, x, y);
-    }
-
-    private int xyTo1D(int N, int x, int y) {
-        return x + y * N;
+        return x + y * this.N;
     }
 
     private void checkRowAndCol(int row, int col) {
@@ -49,9 +37,59 @@ public class Percolation {
         }
     }
 
+    public void testOpen(){
+        //when N == 4
+        open(0,1);
+        open(3,2);
+        open(2,3);
+        open(2,2);
+
+        if(wqu.connected(xyTo1D(0,1),xyTo1D(3,2))==false){
+            System.out.println("test 1 passed");
+        }
+        if(wqu.connected(xyTo1D(3,2),xyTo1D(2,3))==true){
+            System.out.println("test 2 passed");
+        }
+    }
+
     public void open(int row, int col) {
+        // open the site (row, col) if it is not open already
         checkRowAndCol(row, col);
-    }       // open the site (row, col) if it is not open already
+        openHelper[row][col] = isOpened;
+
+        /*
+        Using a 2D array to store the info of opened component
+        if there are two adjacent component, union them
+        */
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1; j++) {
+
+                if (openHelper[i][j] == isOpened) {
+                    int index1 = xyTo1D(i, j);
+
+                    if (openHelper[i + 1][j] == isOpened) {
+                        int index2 = xyTo1D(i + 1, j);
+                        wqu.union(index1, index2);
+                    }
+
+                    if (openHelper[i][j + 1] == isOpened) {
+                        int index2 = xyTo1D(i, j + 1);
+                        wqu.union(index1, index2);
+                    }
+                    /*
+                    since we scan the array from left to right
+                    then scan the array from up to down
+                    we only need to care about the right and down direction
+
+                    for example, if arr[1][2] and arr[1][3] are both 'opened'
+                    after we scan arr[1][2], we have union arr[1][2] and arr[1][3]
+                    so when we scan arr[1][3], we don't need to care about arr[1][2]
+                    */
+                }
+            }
+        }
+
+    }
 
     public boolean isOpen(int row, int col) {
         // is the site (row, col) open?
@@ -83,7 +121,6 @@ public class Percolation {
 
     public static void main(String[] args) {
         Percolation percolation=new Percolation(4);
-        percolation.testXyTo1D();
-
+        percolation.testOpen();
     }
 }
