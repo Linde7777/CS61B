8eaddbbc9d97061b7c86e91445c64d6a63245f0b
LongPeng <longpeng96@qq.com>
2022-03-18 09:10:36 +0800

    get() completed

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -33,41 +33,68 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         this.clear();
     }
 
-    /* Removes all of the mappings from this map. */
+    /* Removes all the mappings from this map. */
     @Override
     public void clear() {
         root = null;
         size = 0;
     }
 
-    /** Returns the value mapped to by KEY in the subtree rooted in P.
-     *  or null if this map contains no mapping for the key.
+    /**
+     * Returns the value mapped to by KEY in the subtree rooted in P.
+     * or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException("key can't be null");
+        }
+
+        if (p == null) return null;
+
+        int cmp = key.compareTo(p.key);
+        if (cmp == 0) {
+            return p.value;
+        }
+        if (cmp > 0) {
+            return getHelper(key, p.right);
+        } else {
+            return getHelper(key, p.left);
+        }
+
     }
 
-    /** Returns the value to which the specified key is mapped, or null if this
-     *  map contains no mapping for the key.
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key,root);
     }
 
-    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
-      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+
+    /**
+     * Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
+     * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return new Node(key, value);
+        }
+
+        if(get(key)!=null){
+
+        }
+
     }
 
-    /** Inserts the key KEY
-     *  If it is already present, updates value to be VALUE.
+    /**
+     * Inserts the key KEY
+     * If it is already present, updates value to be VALUE.
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        putHelper(key, value, root);
     }
 
     /* Returns the number of key-value mappings in this map. */
@@ -84,18 +111,20 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         throw new UnsupportedOperationException();
     }
 
-    /** Removes KEY from the tree if present
-     *  returns VALUE removed,
-     *  null on failed removal.
+    /**
+     * Removes KEY from the tree if present
+     * returns VALUE removed,
+     * null on failed removal.
      */
     @Override
     public V remove(K key) {
         throw new UnsupportedOperationException();
     }
 
-    /** Removes the key-value entry for the specified key only if it is
-     *  currently mapped to the specified value.  Returns the VALUE removed,
-     *  null on failed removal.
+    /**
+     * Removes the key-value entry for the specified key only if it is
+     * currently mapped to the specified value.  Returns the VALUE removed,
+     * null on failed removal.
      **/
     @Override
     public V remove(K key, V value) {
