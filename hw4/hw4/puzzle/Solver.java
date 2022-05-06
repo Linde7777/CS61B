3c65e3ef87ba973ac35887981a0b0cc5cae3bb51
LongPeng <longpeng96@qq.com>
2022-05-06 09:45:59 +0800

    second optimization

--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -5,10 +5,9 @@ import edu.princeton.cs.algs4.MinPQ;
 
 import java.util.HashSet;
 
-
 public class Solver {
     private MinPQ<SearchNode> pq = new MinPQ<>();
-    private int totalmoves = 0;
+    private int totalmoves;
     private HashSet<WorldState> path = new HashSet<>();
     int debugIndex = 1;
 
@@ -53,7 +52,8 @@ public class Solver {
             }
 
             for (WorldState neighborState : formerMinNode.worldState.neighbors()) {
-                if (!neighborState.equals(formerMinNode.worldState)) {
+                if (formerMinNode.prev == null
+                        || !neighborState.equals(formerMinNode.prev.worldState)) {
                     pq.insert(new SearchNode(neighborState, formerMinNode.numMoves + 1, formerMinNode));
                 }
             }
@@ -66,78 +66,3 @@ public class Solver {
     }
 
 }
-
-/*
-public class Solver {
-    private MinPQ<SearchNode> pq=new MinPQ<>();
-    private int totalMoves =0;
-    private ArrayList<WorldState> path=new ArrayList<>();
-
-    public Iterable<WorldState> solution() {
-        ArrayList<WorldState> ret = new ArrayList<>();
-        for (int i = totalMoves; i >= 0; i--) {
-            ret.add(path.get(i));
-        }
-        return ret;
-    }
-
-    private void getAnswer(SearchNode goal) {
-        totalMoves = goal.numMoves;
-        path = new ArrayList<>();
-        SearchNode p = goal;
-        while (p != null) {
-            path.add(p.worldState);
-            p = p.prev;
-        }
-    }
-
-    public int moves() {
-        return totalMoves;
-    }
-
-    private class SearchNode implements Comparable<SearchNode> {
-        WorldState worldState;
-        int numMoves;
-        SearchNode prev;
-        int priority;
-
-        public SearchNode(WorldState worldState, int numMoves, SearchNode prev) {
-            this.worldState = worldState;
-            this.prev = prev;
-            this.numMoves = numMoves;
-            priority = this.numMoves + this.worldState.estimatedDistanceToGoal();
-        }
-
-        @Override
-        public int compareTo(SearchNode searchNode) {
-            return this.priority - searchNode.priority;
-        }
-
-    }
-
-    public Solver(WorldState initial) {
-
-        pq.insert(new SearchNode(initial, 0, null));
-
-        while (true) {
-            SearchNode node = pq.delMin();
-            if (node.worldState.isGoal()) {
-                getAnswer(node);
-                return;
-            }
-
-            for (WorldState neighborState : node.worldState.neighbors()) {
-                if (node.prev == null
-                        || !neighborState.equals(node.prev.worldState)) {
-                    pq.insert(new SearchNode(neighborState, node.numMoves + 1, node));
-                }
-            }
-
-            int temp=1;
-        }
-    }
-
-}
-
-
- */
