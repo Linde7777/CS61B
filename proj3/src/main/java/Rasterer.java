632652f77c41695bf8ecd904d4799307cacdd3ea
LongPeng <longpeng96@qq.com>
2022-05-10 16:47:56 +0800

    rename variables

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -27,33 +27,40 @@ public class Rasterer {
         d0_x0_y0 longitudes -122.29980468 and -122.21191406
         latitudes 37.82280243 and 37.89219554.
 
-        minLon=-122.29980468
+        ROOT_ULLON=-122.29980468
         midLon=(-122.29980468-122.21191406)/2
-        maxLon=-122.21191406
-        d1 x0,y0 minLon and midLon
-        d1 x0,y1 midLon and maxLon
+        ROOT_LRLON=-122.21191406
+        d1 x0,y0 ROOT_ULLON and midLon
+        d1 x0,y1 midLon and ROOT_LRLON
         d1 x1,yi
 
         d1LonDPP=cal(min,mid/1,256)
         d2LonDPP=cal(min,mid/2,256)
         */
+        /*
+        public static final double
+        ROOT_ULLAT = 37.892195547244356,
+        ROOT_ULLON = -122.2998046875,
+        ROOT_LRLAT = 37.82280243352756,
+        ROOT_LRLON = -122.2119140625;
+         */
         final double size = 256;
-        final double minLon = -122.29980468;
-        final double maxLat = 37.892195547244356;
-        final double maxLon = -122.21191406;
-        final double minLat = 37.82280243352756;
+        final double ROOT_ULLON = -122.2998046875;
+        final double ROOT_ULLAT = 37.892195547244356;
+        final double ROOT_LRLON = -122.2119140625;
+        final double ROOT_LRLAT = 37.82280243352756;
 
         /*
-        double singleTileLen1 =(maxLon-minLon)/2;
-        double singleTileLen2=(maxLon-minLon)/4;
-        double singleTileLen3=(maxLon-minLon)/8;
+        double singleTileLen1 =(ROOT_LRLON-ROOT_ULLON)/2;
+        double singleTileLen2=(ROOT_LRLON-ROOT_ULLON)/4;
+        double singleTileLen3=(ROOT_LRLON-ROOT_ULLON)/8;
         double LonDPP1=singleTileLen1/256;
         double LonDPP2=singleTileLen2/256;
         double LonDPP3=singleTileLen3/256;
          */
         int depth = -1;
         for (int i = 1; i < 7; i++) {
-            double singleTileLen = (maxLon - minLon) / Math.pow(2, i);
+            double singleTileLen = (ROOT_LRLON - ROOT_ULLON) / Math.pow(2, i);
             double LonDPP = singleTileLen / size;
             if (LonDPP < paraLonDPP) {
                 depth = i;
@@ -68,14 +75,14 @@ public class Rasterer {
         /*
         if depth==3
         len=2^3=8
-        x0_leftBorder=minLon+(maxLon-minLon)*(0/8)
-        x1_leftBorder=minLon+(maxLon-minLon)*(1/8)
-        x2_leftBorder=minLon+(maxLon-minLon)*(2/8)
-        x3_leftBorder=minLon+(maxLon-minLon)*(3/8)
+        x0_leftBorder=ROOT_ULLON+(ROOT_LRLON-ROOT_ULLON)*(0/8)
+        x1_leftBorder=ROOT_ULLON+(ROOT_LRLON-ROOT_ULLON)*(1/8)
+        x2_leftBorder=ROOT_ULLON+(ROOT_LRLON-ROOT_ULLON)*(2/8)
+        x3_leftBorder=ROOT_ULLON+(ROOT_LRLON-ROOT_ULLON)*(3/8)
         */
         double[] leftBorders = new double[len];
         for (int i = 0; i < len; i++) {
-            leftBorders[i] = minLon + (maxLon - minLon) * ((double) i / len);
+            leftBorders[i] = ROOT_ULLON + (ROOT_LRLON - ROOT_ULLON) * ((double) i / len);
         }
         double raster_ul_lon = -1;
         int ullonTileIndex = -1;
@@ -91,14 +98,14 @@ public class Rasterer {
         /*
         if depth==3
         len=2^3=8
-        x0_rightBorder=x0_leftBorder+(maxLon-minLon)/8
-        x1_rightBorder=x1_leftBorder+(maxLon-minLon)/8
-        x2_rightBorder=x2_leftBorder+(maxLon-minLon)/8
-        x3_rightBorder=x3_leftBorder+(maxLon-minLon)/8
+        x0_rightBorder=x0_leftBorder+(ROOT_LRLON-ROOT_ULLON)/8
+        x1_rightBorder=x1_leftBorder+(ROOT_LRLON-ROOT_ULLON)/8
+        x2_rightBorder=x2_leftBorder+(ROOT_LRLON-ROOT_ULLON)/8
+        x3_rightBorder=x3_leftBorder+(ROOT_LRLON-ROOT_ULLON)/8
          */
         double[] rightBorders = new double[len];
         for (int i = 0; i < len; i++) {
-            rightBorders[i] = leftBorders[i] + (maxLon - minLon) / len;
+            rightBorders[i] = leftBorders[i] + (ROOT_LRLON - ROOT_ULLON) / len;
         }
         double raster_lr_lon = -1;
         int lrlonTileIndex = -1;
@@ -114,22 +121,22 @@ public class Rasterer {
         /*
         if depth==3
         len=2^3=8
-        y0_upBorder=minLat+(maxLat-minLat)*(0/8)
-        y1_upBorder=minLat+(maxLat-minLat)*(1/8)
-        y2_upBorder=minLat+(maxLat-minLat)*(2/8)
-        y3_upBorder=minLat+(maxLat-minLat)*(3/8)
+        y0_upBorder=ROOT_LRLAT+(ROOT_ULLAT-ROOT_LRLAT)*(0/8)
+        y1_upBorder=ROOT_LRLAT+(ROOT_ULLAT-ROOT_LRLAT)*(1/8)
+        y2_upBorder=ROOT_LRLAT+(ROOT_ULLAT-ROOT_LRLAT)*(2/8)
+        y3_upBorder=ROOT_LRLAT+(ROOT_ULLAT-ROOT_LRLAT)*(3/8)
         */
         double[] upBorders = new double[len];
         for (int i = 0; i < len; i++) {
-            upBorders[i] = minLat + (maxLat - minLat) * ((double) i / len);
+            upBorders[i] = ROOT_LRLAT + (ROOT_ULLAT - ROOT_LRLAT) * ((double) i / len);
         }
         double raster_ul_lat = -1;
         int ullatTileIndex = -1;
         for (int i = 0; i < len - 1; i++) {
-            if (paraUllat <= upBorders[i+1]
+            if (paraUllat <= upBorders[i + 1]
                     && paraUllat > upBorders[i]) {
-                raster_ul_lat = upBorders[i+1];
-                ullatTileIndex = i+1;
+                raster_ul_lat = upBorders[i + 1];
+                ullatTileIndex = i + 1;
                 break;
             }
         }
@@ -137,19 +144,19 @@ public class Rasterer {
         /*
         if len==3
         len=2^3=8
-        y0_lowBorder=y0_upBorder+(maxLat-minLat)/8
-        y1_lowBorder=y1_upBorder+(maxLat-minLat)/8
-        y2_lowBorder=y2_upBorder+(maxLat-minLat)/8
-        y3_lowBorder=y3_upBorder+(maxLat-minLat)/8
+        y0_lowBorder=y0_upBorder+(ROOT_ULLAT-ROOT_LRLAT)/8
+        y1_lowBorder=y1_upBorder+(ROOT_ULLAT-ROOT_LRLAT)/8
+        y2_lowBorder=y2_upBorder+(ROOT_ULLAT-ROOT_LRLAT)/8
+        y3_lowBorder=y3_upBorder+(ROOT_ULLAT-ROOT_LRLAT)/8
          */
         double[] lowBorders = new double[len];
         for (int i = 0; i < len; i++) {
-            lowBorders[i] = upBorders[i] + (maxLat - minLat) / len;
+            lowBorders[i] = upBorders[i] + (ROOT_ULLAT - ROOT_LRLAT) / len;
         }
         double raster_lr_lat = -1;
         int lrlatTileIndex = -1;
         for (int i = 0; i < len - 1; i++) {
-            if (paraLrlat <= lowBorders[i+1]
+            if (paraLrlat <= lowBorders[i + 1]
                     && paraLrlat > lowBorders[i]) {
                 raster_lr_lat = lowBorders[i];
                 lrlatTileIndex = i;
