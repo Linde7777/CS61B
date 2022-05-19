0cb5589a7bc032074e38da285993600780b53e15
LongPeng <longpeng96@qq.com>
2022-05-19 11:45:19 +0800

    detect circle, v1

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -19,6 +19,26 @@ public class MazeCycles extends MazeExplorer {
         // TODO: Your code here!
     }
 
-    // Helper methods go here
+    private void dfs(int v){
+        distTo[v]=0;
+        edgeTo[v]=0;
+        dfsHelper(v);
+    }
+
+    private void dfsHelper(int v){
+        marked[v]=true;
+        announce();
+
+        for(int w:maze.adj(v)){
+            //TODO: detect circle
+            if(!marked[w]){
+                distTo[w]=distTo[v]+1;
+                edgeTo[w]=v;
+                marked[w]=true;
+                announce();
+                dfsHelper(w);
+            }
+        }
+    }
 }
 
