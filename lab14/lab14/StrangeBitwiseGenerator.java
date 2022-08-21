b90e7c14c74d5e24457aaad49c73a15fe9873152
LongPeng <longpeng96@qq.com>
2022-08-21 15:55:48 +0800

    optimize code

--- lab14/lab14/StrangeBitwiseGenerator.java
+++ lab14/lab14/StrangeBitwiseGenerator.java
@@ -2,11 +2,13 @@ package lab14;
 
 import lab14lib.Generator;
 
-public class SawToothGenerator implements Generator {
+
+public class StrangeBitwiseGenerator implements Generator {
     private final int period;
     private int state;
+    private int weirdState;
 
-    public SawToothGenerator(int period) {
+    public StrangeBitwiseGenerator(int period) {
         state = 0;
         this.period = period;
     }
@@ -14,20 +16,21 @@ public class SawToothGenerator implements Generator {
     @Override
     public double next() {
         state = (state + 1);
+        weirdState = state & (state >>> 3) % period;
         return normalize();
     }
 
     private double normalize() {
         int minState = 0;
         int maxState = period - 1;
-        if (state > maxState) {
-            state = state % period;
+        if (weirdState > maxState) {
+            weirdState = weirdState % period;
         }
         double minValue = -1.0;
         double maxValue = 1.0;
         double magnification = (maxState - minState) / (maxValue - minValue);
         double offset = 1;
 
-        return (state / magnification) - offset;
+        return (weirdState / magnification) - offset;
     }
 }
