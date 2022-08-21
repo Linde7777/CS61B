94081799048d66d6aa395c6fa2d8d95378cdc281
LongPeng <longpeng96@qq.com>
2022-08-21 15:20:43 +0800

    v1,finished

---
+++ lab14/lab14/AcceleratingSawToothGenerator.java
@@ -0,0 +1,39 @@
+package lab14;
+
+import lab14lib.Generator;
+
+public class AcceleratingSawToothGenerator implements Generator {
+    private int state;
+    private int period;
+    private final double factor;
+    private boolean firstRun = true;
+
+    public AcceleratingSawToothGenerator(int period, double factor) {
+        state = 0;
+        this.period = period;
+        this.factor = factor;
+    }
+
+    @Override
+    public double next() {
+        state = (state + 1);
+        return normalize();
+    }
+
+    private double normalize() {
+        //TODO: modify state-> this.state
+        if (this.state > this.period - 1) {
+            this.state = this.state % this.period;
+            this.period = (int) (this.period * factor);
+        }
+        int minState = 0;
+        int maxState = this.period - 1;
+        double minValue = -1.0;
+        double maxValue = 1.0;
+        double magnification = (maxState - minState) / (maxValue - minValue);
+        int offset = 1;
+        firstRun = false;
+
+        return (this.state / magnification) - offset;
+    }
+}
--- lab14/lab14/Main.java
+++ lab14/lab14/Main.java
@@ -7,13 +7,14 @@ import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args) {
 		/*
-		Generator generator = new SineWaveGenerator(200);
-		GeneratorDrawer gd = new GeneratorDrawer(generator);
-		gd.draw(4096);
-
-		 */
 		Generator generator = new SawToothGenerator(512);
 		GeneratorAudioVisualizer gav = new GeneratorAudioVisualizer(generator);
 		gav.drawAndPlay(4096, 1000000);
+
+		 */
+		Generator generator = new AcceleratingSawToothGenerator(200, 1.1);
+		GeneratorAudioVisualizer gav = new GeneratorAudioVisualizer(generator);
+		gav.drawAndPlay(4096, 1000000);
+
 	}
 } 
