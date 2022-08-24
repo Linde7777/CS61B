36b5ec3d19c4ede0b7b5494b7bd755718ee98a29
LongPeng <longpeng96@qq.com>
2022-08-24 18:06:35 +0800

    add class

--- lab14/lab14/Main.java
+++ lab14/lab14/Main.java
@@ -5,16 +5,16 @@ import lab14lib.*;
 import java.util.ArrayList;
 
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		/*
 		Generator generator = new SawToothGenerator(512);
 		GeneratorAudioVisualizer gav = new GeneratorAudioVisualizer(generator);
 		gav.drawAndPlay(4096, 1000000);
 		 */
 
-		Generator generator = new StrangeBitwiseGenerator(200);
-		GeneratorAudioVisualizer gav = new GeneratorAudioVisualizer(generator);
-		gav.drawAndPlay(4096, 1000000);
+        Generator generator = new StrangeBitwiseGenerator(200);
+        GeneratorAudioVisualizer gav = new GeneratorAudioVisualizer(generator);
+        gav.drawAndPlay(4096, 1000000);
 
-	}
+    }
 } 
