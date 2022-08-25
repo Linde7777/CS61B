6c73dcd8a60858208a644317930136ee2e2fd554
LongPeng <longpeng96@qq.com>
2022-08-25 20:27:01 +0800

    basic functions

--- lab15/creatures/Clorus.java
+++ lab15/creatures/Clorus.java
@@ -1,4 +1,71 @@
 package creatures;
 
-public class Clorus {
+import huglife.Action;
+import huglife.Creature;
+import huglife.Direction;
+import huglife.Occupant;
+
+import java.awt.*;
+import java.util.Map;
+
+public class Clorus extends Creature {
+    /**
+     * red color.
+     */
+    private int r;
+    /**
+     * green color.
+     */
+    private int g;
+    /**
+     * blue color.
+     */
+    private int b;
+
+    /**
+     * creates plip with energy equal to E.
+     */
+    public Clorus(double e) {
+        super("clorus");
+        r = 0;
+        g = 0;
+        b = 0;
+        energy = e;
+    }
+
+    @Override
+    public void move() {
+        energy -= 0.03;
+    }
+
+    @Override
+    public void attack(Creature c) {
+        energy += c.energy();
+    }
+
+    @Override
+    public Creature replicate() {
+        Clorus offspring = new Clorus(0.5 * energy);
+        this.energy *= 0.5;
+        return offspring;
+    }
+
+    @Override
+    public void stay() {
+        energy -= 0.01;
+    }
+
+    @Override
+    public Action chooseAction(Map<Direction, Occupant> neighbors) {
+        return null;
+    }
+
+    @Override
+    public Color color() {
+        return new Color(34, 0, 231);
+    }
+
+    public String name() {
+        return "clorus";
+    }
 }
