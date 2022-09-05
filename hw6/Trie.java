70b9c679caaf0f7ee8bb650568786e7b3b6413d9
LongPeng <longpeng96@qq.com>
2022-09-05 14:33:33 +0800

    v1

--- hw6/Trie.java
+++ hw6/Trie.java
@@ -2,37 +2,54 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Trie {
-    private static final int R=128;
+    private static final int R = 128;
 
-    private class Node{
+    private class Node {
         boolean exists;
         Node[] links;
 
-        public Node(){
-            links=new Node[R];
-            exists=false;
+        public Node() {
+            links = new Node[R];
+            exists = false;
         }
     }
 
-    private Node root=new Node();
+    private Node root = new Node();
 
-    public void put(String key){
-        put(root,key,0);
+    public void put(String key) {
+        put(root, key, 0);
     }
 
-    private Node put(Node x,String key,int d){
-        if(x==null){
-            x=new Node();
+    private Node put(Node x, String key, int index) {
+        if (x == null) {
+            x = new Node();
         }
 
-        if(d==key.length()){
-            x.exists=true;
+        if (index == key.length()) {
+            x.exists = true;
             return x;
         }
 
-        char c=key.charAt(d);
-        x.links[c]=put(x.links[c],key,d+1);
+        char c = key.charAt(index);
+        x.links[c] = put(x.links[c], key, index + 1);
 
         return x;
+    }
+
+    public boolean findKey(String key) {
+        return findKey(root, key, 0);
+    }
+
+    private boolean findKey(Node x, String key, int index) {
+        if (index + 1 == key.length() && x.exists) {
+            return true;
+        }
+
+        char c = key.charAt(index);
+        if (x.links[c] != null) {
+            return findKey(x.links[c], key, index + 1);
+        }
+
+        return false;
     }
 }
