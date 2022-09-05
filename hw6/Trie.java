1ade5d4a9649bfe5a57c36dff37fdf3e6b78ed07
LongPeng <longpeng96@qq.com>
2022-09-05 14:03:57 +0800

    create class

---
+++ hw6/Trie.java
@@ -0,0 +1,38 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Trie {
+    private static final int R=128;
+
+    private class Node{
+        boolean exists;
+        Node[] links;
+
+        public Node(){
+            links=new Node[R];
+            exists=false;
+        }
+    }
+
+    private Node root=new Node();
+
+    public void put(String key){
+        put(root,key,0);
+    }
+
+    private Node put(Node x,String key,int d){
+        if(x==null){
+            x=new Node();
+        }
+
+        if(d==key.length()){
+            x.exists=true;
+            return x;
+        }
+
+        char c=key.charAt(d);
+        x.links[c]=put(x.links[c],key,d+1);
+
+        return x;
+    }
+}
