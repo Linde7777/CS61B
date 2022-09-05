2e8cc8eb5ca251f316fb124dd310f787b2e709e9
LongPeng <longpeng96@qq.com>
2022-09-05 14:16:13 +0800

    v1

--- hw6/Boggle.java
+++ hw6/Boggle.java
@@ -1,22 +1,36 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Boggle {
-    
+
     // File path of dictionary file
     static String dictPath = "words.txt";
 
     /**
      * Solves a Boggle puzzle.
-     * 
-     * @param k The maximum number of words to return.
+     *
+     * @param k             The maximum number of words to return.
      * @param boardFilePath The file path to Boggle board file.
      * @return a list of words found in given Boggle board.
-     *         The Strings are sorted in descending order of length.
-     *         If multiple words have the same length,
-     *         have them in ascending alphabetical order.
+     * The Strings are sorted in descending order of length.
+     * If multiple words have the same length,
+     * have them in ascending alphabetical order.
      */
     public static List<String> solve(int k, String boardFilePath) {
         // YOUR CODE HERE
-        return null;
+        List<String> result = new ArrayList<>();
+        Trie trie = new Trie();
+        In board = new In(boardFilePath);
+        In dict = new In(dictPath);
+        trie.put(board.readAll());
+        while (dict.hasNextLine()) {
+            String key = dict.readLine();
+            if (trie.findKey(key)) {
+                result.add(key);
+            }
+        }
+        return result;
     }
+
+
 }
