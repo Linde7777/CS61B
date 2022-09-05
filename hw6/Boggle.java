fab89f734ee75fe7e686f66af528186c72eda9c0
LongPeng <longpeng96@qq.com>
2022-09-05 15:16:43 +0800

    createBoardArray() finished

--- hw6/Boggle.java
+++ hw6/Boggle.java
@@ -18,6 +18,7 @@ public class Boggle {
      */
     public static List<String> solve(int k, String boardFilePath) {
         // YOUR CODE HERE
+        /*
         List<String> result = new ArrayList<>();
         Trie trie = new Trie();
         In board = new In(boardFilePath);
@@ -30,7 +31,28 @@ public class Boggle {
             }
         }
         return result;
+         */
+
+        List<String> result=new ArrayList<>();
+        In boardStream=new In(boardFilePath);
+        In dictStream=new In(dictPath);
+        Trie trieOfDict=new Trie();
+        trieOfDict.put(dictStream.readAll());
+
+        boardStream.close();
+        dictStream.close();
+        return result;
     }
 
+    public char[][] createBoardArray(In boardStream){
+        int width=boardStream.readLine().length();
+        int height=1;
+        while(boardStream.hasNextLine()){
+            height+=1;
+            boardStream.readLine();
+        }
+        char[][] board=new char[width][height];
+        return board;
+    }
 
 }
